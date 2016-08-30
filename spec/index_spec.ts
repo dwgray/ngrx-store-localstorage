10090fedea871102b1b7bf7c0c3fb4b710175a63
Music 4 Dance <david@music4dance.net>
2016-08-30 14:56:03 -0700

    Get first trivial jasmine test up and running

---
+++ spec/index_spec.ts
@@ -0,0 +1,10 @@
+declare var describe, expect, it;
+
+// require('es6-shim');
+// require('reflect-metadata');
+
+describe('ngrx-local-storage', () => {
+    it('should just work', () => {
+        expect(true).toBe(true);
+    });
+});
