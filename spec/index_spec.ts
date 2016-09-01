418d501759e58bf495311e620e05fb5bf1755ad4
Music 4 Dance <david@music4dance.net>
2016-09-01 09:44:04 -0700

    Get test build working again
    Jasmine doen't appear to be executing, can't figure out how to diagnose, so taking a step back...

--- spec/index_spec.ts
+++ spec/index_spec.ts
@@ -1,8 +1,5 @@
 declare var describe, expect, it;
 
-// require('es6-shim');
-// require('reflect-metadata');
-
 describe('ngrx-local-storage', () => {
     it('should just work', () => {
         expect(true).toBe(true);
